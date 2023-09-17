9ecba89963d1d454fda8e54a4ab364d4e68d28ea
as1gh08 <116599123+as1gh08@users.noreply.github.com>
2023-09-18 00:56:20 +0300

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from main import PyWeather
-
-pw = PyWeather("Teofipol", "831da1b485bc4e179f3132157231609")
-print(pw.get_local_time())
-print(pw.get_temperature(celsius=True))
-print(pw.get_weather())
