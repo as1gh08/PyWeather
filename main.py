ef9fb8e327f3e67f1f15b30d7ad949fb6ba53fad
as1gh08 <as1sava54@gmail.com>
2023-09-18 00:46:27 +0300

    update 1.1

--- main.py
+++ main.py
@@ -1,42 +1,32 @@
 import requests
+from typing import Optional
 
 class PyWeather:
     def __init__(self, city: str, api: str):
         self.city = city
         self.api = api
-        self.response = None
+        self.response = self.get_response()
 
-    def get_weather(self) -> dict:
+    def get_response(self) -> dict:
         if self.response is None:
             url = f"http://api.weatherapi.com/v1/current.json?key={self.api}&q={self.city}&aqi=no"
             self.response = requests.get(url).json()
-
         return self.response
     
-    def get_temperature(self, celsius: bool = True) -> int:
-        weather_data = self.get_weather()
-        
-        if 'current' in weather_data:
-            current_data = weather_data['current']
-            if celsius:
-                return current_data.get('temp_c')
-            else:
-                return current_data.get('temp_f')
-        
+    def get_weather(self) -> Optional[dict]:
+        return self.response.get("current", None)
+    
+    def get_location(self) -> Optional[dict]:
+        return self.response.get("location", None)
+    
+    def get_temperature(self, celsius: bool = True) -> Optional[int]:
+        current_data = self.response.get("current", None)
+        if current_data:
+            return current_data.get('temp_c' if celsius else 'temp_f', None)
         return None
     
-    def get_location(self) -> dict:
-        location_data = self.get_weather()
-        if "location" in location_data:
-            return location_data["location"]
-
+    def get_local_time(self) -> Optional[str]:
+        location_data = self.response.get("location", None)
+        if location_data:
+            return type(location_data.get("localtime", None))
         return None
-    
-    def get_local_time(self):
-        local_time_data = self.get_weather()
-        if "location" in local_time_data:
-            local_time_data = local_time_data['location']
-            return local_time_data["localtime"]
-
-
-        
