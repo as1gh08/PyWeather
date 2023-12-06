d34a79c85b86a3bc0055515bbf99836b66ded9d4
as1gh08 <116599123+as1gh08@users.noreply.github.com>
2023-12-06 21:04:48 +0200

    Update pyweather.py

--- pyweather.py
+++ pyweather.py
@@ -5,7 +5,7 @@ class PyWeather:
     def __init__(self, city: str, api: str):
         self.city = city
         self.api = api
-        self.response = self.get_response()
+        self.response = None
 
     def get_response(self) -> dict:
         if self.response is None:
@@ -14,19 +14,23 @@ class PyWeather:
         return self.response
     
     def get_weather(self) -> Optional[dict]:
+        self.get_response()
         return self.response.get("current", None)
     
     def get_location(self) -> Optional[dict]:
+        self.get_response()
         return self.response.get("location", None)
     
     def get_temperature(self, celsius: bool = True) -> Optional[int]:
+        self.get_response()
         current_data = self.response.get("current", None)
         if current_data:
             return current_data.get('temp_c' if celsius else 'temp_f', None)
         return None
     
     def get_local_time(self) -> Optional[str]:
+        self.get_response()
         location_data = self.response.get("location", None)
         if location_data:
-            return type(location_data.get("localtime", None))
+            return location_data.get("localtime", None)
         return None
